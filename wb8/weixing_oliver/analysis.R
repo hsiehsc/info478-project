26beb8adf19a3bc22f0867ca73334d1d759fbacb
weixingnie <wn5@uw.edu>
2019-05-20 18:17:07 -0700

    I done

--- wb8/weixing_oliver/analysis.R
+++ wb8/weixing_oliver/analysis.R
@@ -1,4 +1,14 @@
 library(tidyverse)
 library(plotly)
-
+library(dplyr)
+library(ggplot2)
 read.csv("minimum_wage_data.csv")
+
+HW_WA_CA_minimum_wage <- minimum_wage %>%
+                        filter(State == "Hawaii" | State == "Washington" | State == "California")
+
+p <- ggplot(HW_WA_CA_minimum_wage, aes(x= State, y= Low.2018)) + 
+      geom_violin(trim = FALSE)
+p + stat_summary(fun.y = mean, geom = "point", size = 1, color = "red")
+p + geom_boxplot(width=0.1)
+ggsave(filename = "Oliver_Nie_data.png")
