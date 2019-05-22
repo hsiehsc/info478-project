c0329ab49c9d70ce629859da7030c5e38106cd94
Steven Hsieh <hsiehsc@uw.edu>
2019-05-22 12:31:24 -0700

    Worked on leaflet and finished wb8

---
+++ leaflet/analysis.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(leaflet)
+library(tigris)
+
+states <- states()
+
+states %>% 
+  leaflet() %>% 
+  addTiles() %>% 
+  addPolygons(popup=~NAME)
+
+data <- read.csv("minimum_wage_data.csv", stringsAsFactors = F)
+
+data_2017 <- data %>% 
+  filter(Year == "2017") %>% 
+  
+states_merged <- geo_join(states, data_2017, "NAME", "State")
+
+pal <- colorNumeric("Greens", domain=states_merged$Low.Value)
+
+
+states_merged <- subset(states_merged, !is.na(Low.Value))
+
+popup <- paste0("<strong>", states_merged$NAME, 
+                "</strong><br />Total: ", states_merged$Low.Value)
+
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  setView(-98.483330, 38.712046, zoom = 4) %>% 
+  addPolygons(data = states_merged , 
+              fillColor = ~pal(states_merged$Low.Value), 
+              fillOpacity = 0.7, 
+              weight = 0.2, 
+              smoothFactor = 0.2, 
+              highlight = highlightOptions(
+                weight = 5,
+                color = "#666",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label=popup,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto")) %>% 
+  addLegend(pal = pal, 
+            values = states_merged$Low.Value, 
+            position = "bottomright", 
+            title = "Minimum Wage ($)")
--- wb8/steven_h/analysis.R
+++ wb8/steven_h/analysis.R
@@ -1,4 +1,18 @@
 library(tidyverse)
 library(plotly)
 
-read.csv("minimum_wage_data.csv")
+data <- read.csv("minimum_wage_data.csv", stringsAsFactors = F)
+
+washington_data <- data %>% 
+  filter(State == "Washington")
+
+p <- plot_ly(data = washington_data, x = ~Year, y =~Low.2018) %>% 
+  layout(title = paste0("Minimum wage in Washington over Time", 
+                        " converted to 2018 dollars"))
+
+plot_ly(data = washington_data, x = ~CPI.Average, y =~Low.2018) %>% 
+  layout(title = "CPI in Washington State Over Time")
+
+orca(p, file = "min_wa_wage_adjusted.png")
+
+
