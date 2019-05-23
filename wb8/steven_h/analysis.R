c307d3586a93246c70f59b1dc3b8e968c6975de6
Steven Hsieh <hsiehsc@uw.edu>
2019-05-23 15:54:35 -0700

    updated min wage graph

--- wb8/steven_h/analysis.R
+++ wb8/steven_h/analysis.R
@@ -6,9 +6,12 @@ data <- read.csv("minimum_wage_data.csv", stringsAsFactors = F)
 washington_data <- data %>% 
   filter(State == "Washington")
 
+
 p <- plot_ly(data = washington_data, x = ~Year, y =~Low.2018) %>% 
   layout(title = paste0("Minimum wage in Washington over Time", 
-                        " converted to 2018 dollars"))
+                        " converted to 2018 dollars"), yaxis = list(
+                          title = "Adjusted Minimum Wage"
+                        ))
 
 plot_ly(data = washington_data, x = ~CPI.Average, y =~Low.2018) %>% 
   layout(title = "CPI in Washington State Over Time")
