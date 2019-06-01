c9488458ec7e25abfd91405b939213c68d92fb26
weixingnie <wn5@uw.edu>
2019-05-31 17:55:05 -0700

    Oliver added a few change

---
+++ trial/analysis.R
@@ -0,0 +1,28 @@
+library(foreign)
+library(survey)
+library(Hmisc)
+library(dplyr)
+library(ggplot2)
+library(knitr)
+
+
+LLC2017_data <- LLC2017_data %>%
+                  filter((x.state <= 56))
+LLC2017_data <- LLC2017_data %>%
+                  select(x.state, x.rfbmi5, x.llcpwt, x.psu)
+LLC2017_data <- LLC2017_data %>%
+                  mutate(
+                    x.rfbmi5 = replace(x.rfbmi5, x.rfbmi5 == 9, NA),
+                    x.rfbmi5 = replace(x.rfbmi5, x.rfbmi5 == 1, 0),
+                    x.rfbmi5 = replace(x.rfbmi5, x.rfbmi5 == 2, 1)
+                  )
+Obesity_2017_data <- svydesign(
+                            weights = ~x.llcpwt,
+                            data = LLC2017_data,
+                            id = ~x.psu,
+                            nest = TRUE
+                          )
+
+svyby(~x.rfbmi5, ~x.state, Obesity_2017_data, svymean, na.rm = T) -> result_2017
+svymean(~x.rfbmi5, Obesity_2017_data, na.rm = T) -> mean_2017
+write.csv(result_2017, "2017overweightandobesity.csv")
