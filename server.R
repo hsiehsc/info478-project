d118bfde96fbff7b6f176aae3ee97477525433f4
Steven Hsieh <hsiehsc@uw.edu>
2019-06-04 19:45:42 -0700

    Added introduction page, wrote a bunch of analysis, added in about us page

--- server.R
+++ server.R
@@ -1,108 +1,128 @@
-state_list <- state.name
-data <- read.csv("data/prepped_minimum_wage.csv", stringsAsFactors = F)
-minimum_wage <- data
-homicide_raw <- read.csv("data/homicide_rates.csv", stringsAsFactors = F,
-                         check.names = F)
-all_data <- read.csv("data/alldata.csv", stringsAsFactors = F)
-all_data <- all_data %>%
-  filter(x.state != 11)
-all_data$X <- state.name
-
-minimum_wage <- read.csv("data/prepped_minimum_wage.csv", stringsAsFactors = F)
-minimum_wage <- minimum_wage %>%
-  filter(Year >= 2001)
-minimum_wage <- minimum_wage %>%
-  filter(State != "District of Columbia")
-minimum_wage <- minimum_wage %>%
-  filter(State != "Federal (FLSA)" & State != "Guam" & 
-           State != "Puerto Rico" & State != "U.S. Virgin Islands")
-
 shinyServer(function(input, output) {
-    ### plotly choropleth ####
-    output$plotlymap <- renderPlotly({
-        data_select <- data %>% 
-            filter(Year == input$year_slider)
-        data_select$hover <- with(data_select, paste(State, '<br>',
-                                                     "High Value:", High.Value,
-                                                     "<br>", "2018 Adj Low Value:", Low.2018,
-                                                     '<br>', "2018 Adj High Value:", High.2018
-        ))
-        data_select$StateAbb <- state.abb[match(data_select$State, state.name)]
-        # give state boundaries a white border
-        l <- list(color = toRGB("white"), width = 2)
-        # specify some map projection/options
-        g <- list(
-            scope = 'usa',
-            projection = list(type = 'albers usa'),
-            showlakes = FALSE,
-            lakecolor = toRGB('white')
+  ### plotly choropleth ####
+  output$plotlymap <- renderPlotly({
+    # Setting filter that matches with the UI
+    data_select <- data %>%
+      filter(Year == input$year_slider)
+    # Hover information for the Plotly choropleth
+    data_select$hover <- with(data_select, paste(
+      State, "<br>",
+      "Low Value:", Low.Value,
+      "<br>", "2018 Adj Low Value:", Low.2018,
+      "<br>", "2018 Adj High Value:", High.2018
+    ))
+    # Changing full State names to just their abbreviation
+    data_select$StateAbb <- state.abb[match(data_select$State, state.name)]
+    # Giving state boundaries a white border
+    l <- list(color = toRGB("white"), width = 2)
+    # Specify map projection options
+    g <- list(
+      scope = "usa",
+      projection = list(type = "albers usa"),
+      showlakes = FALSE,
+      lakecolor = toRGB("white")
+    )
+    # Creating Choropleth with the main color of green
+    plot_geo(data_select, locationmode = "USA-states") %>%
+      add_trace(
+        z = ~High.Value, text = ~hover, locations = ~StateAbb,
+        color = ~Low.Value, colors = "Greens"
+      ) %>%
+      colorbar(title = "Minimum Wage") %>%
+      layout(
+        title = "Minimum Wage by State<br>(Hover for breakdown)",
+        geo = g
+      )
+  })
+  ##### end plotly choropleth #####
+
+  ##### homicide #####
+  # Melting homicide dataframe to better match minimum wage format
+  homicide <- melt(homicide_raw, id = "Year")
+  # Omitting NA values that were created
+  homicide <- na.omit(homicide)
+  # Rename column so that homicide and minimum wage can be joined
+  homicide <- homicide %>%
+    rename(State = variable)
+  # Filtering Homicide by the UI input
+  homicide_select <- reactive({
+    homicide %>%
+      filter(State == input$state_text_name)
+  })
+
+  output$homicide_graph <- renderPlotly({
+    # Creating Plotly for Homicide
+    plot_ly(data = homicide_select(),
+            type = "scatter", x = ~Year, y = ~value) %>%
+      layout(
+        title = paste0(
+          "Homicide Rates per 100,000 people in ",
+          input$state_text_name
+        ),
+        xaxis = list(title = "Year"), yaxis = list(
+          title =
+            "Homicide Rates per 100,000"
         )
-        plot_geo(data_select, locationmode = 'USA-states') %>%
-            add_trace(
-                z = ~High.Value, text = ~hover, locations = ~StateAbb,
-                color = ~Low.Value, colors = 'Greens'
-            ) %>%
-            colorbar(title = "Minimum Wage") %>%
-            layout(
-                title = 'Minimum Wage by State<br>(Hover for breakdown)',
-                geo = g
-            )
-        
-})
-    ##### end plotly choropleth #####
-    
-    ##### homicide #####
-    homicide <- melt(homicide_raw, id = "Year")
-    homicide <- na.omit(homicide)
-    homicide <- homicide %>%
-                rename(State = variable)
-    homicide_select <- reactive({homicide %>%
-        filter(State == input$state_text_name)})
+      )
+  })
+  ##### minimum wage and homicide rates #####
+  # Combining Homicide Wage and Minimum Wage
+  homicide_wage <- left_join(homicide, data)
+  homicide_wage <- na.omit(homicide_wage)
 
-    output$homicide_graph <- renderPlotly({
-      plot_ly(data = homicide_select(), type = "scatter", x=~Year, y=~value) %>%
-        layout(title = paste0("Homicide Rates per 100,000 people in ",
-                              input$state_text_name),
-               xaxis = list(title = "Year"), yaxis = list(title =
-                                      "Homicide Rates per 100,000"))
-})
-    ##### minimum wage and homicide rates #####
-    homicide_wage <- left_join(homicide, data)
-    homicide_wage <- na.omit(homicide_wage)
-    
-    hom_wage_select <- reactive({homicide_wage %>% 
-        filter(State == input$state_hom_wage)})
-    
-    x_hom_wage <- "Minimum Wage in U.S. Dollars (2018 Adjusted)"
-    y_hom_wage <- "Homicide Rates per 100,000 people"
-    output$p_hom_wage <- renderPlotly({
-      plot_ly(data = hom_wage_select(), type = "scatter", x=~High.2018,
-              y=~value) %>% 
-        layout(xaxis = list(title =
-                "Minimum Wage in U.S. Dollars (2018 Adjusted)"),
-               yaxis = list(title =
-                "Homicide Rates per 100,000 people"),
-               title = paste0("Minimum Wage (2018 Adjusted) vs. Homicide",
-                              " rates for ", input$state_hom_wage))
-    })
+  # Filtering based on the UI
+  hom_wage_select <- reactive({
+    homicide_wage %>%
+      filter(State == input$state_text_name)
+  })
 
-    
-    final_data <- left_join(minimum_wage, all_data, by = c("State" = "X", "Year" = "Year"))
-    final_data <- final_data %>%
-      select(State, Year, x.rfbmi2, High.2018)
-    names(final_data)[3] <- "Percentage"
-    names(final_data)[4] <- "minimum Wage"
-#    if(!is.null(input$target)) {
-      reactive(final_data <- final_data[final_data$State %in% input$target, ])
-#    }
-    
-    
-    output$statee <- renderPlotly({
-      plot_ly(final_data, x = ~Year, y = ~State, z = ~`minimum Wage`, color = ~Percentage) %>%
-      layout(title = "3D plot of Obesity and overweight VS Year VS Minimum wage") %>%
+  # Creating Plotly
+  output$p_hom_wage <- renderPlotly({
+    plot_ly(
+      data = hom_wage_select(), type = "scatter", x = ~High.2018,
+      y = ~value
+    ) %>%
+      layout(
+        xaxis = list(
+          title =
+            "Minimum Wage in U.S. Dollars (2018 Adjusted)"
+        ),
+        yaxis = list(
+          title =
+            "Homicide Rates per 100,000 people"
+        ),
+        title = paste0(
+          "Minimum Wage (2018 Adjusted) vs. Homicide",
+          " rates in ", input$state_text_name
+        )
+      )
+  })
+
+
+  final_data <- left_join(minimum_wage, all_data,
+    by =
+      c("State" = "X", "Year" = "Year")
+  )
+  final_data <- final_data %>%
+    select(State, Year, x.rfbmi2, High.2018)
+  names(final_data)[3] <- "Percentage"
+  names(final_data)[4] <- "minimum Wage"
+  #    if(!is.null(input$target)) {
+  reactive(final_data <- final_data[final_data$State %in% input$target, ])
+  #    }
+
+
+  output$statee <- renderPlotly({
+    plot_ly(final_data,
+      x = ~Year, y = ~State,
+      z = ~`minimum Wage`, color = ~Percentage
+    ) %>%
+      layout(title = "3D plot of Obesity Rates vs Year vs Minimum Wage") %>%
       add_markers() %>%
-      layout(scene = list(xaxis = list(title = 'Years (2001 --- 2017'),
-                          yaxis = list(title = 'States'),
-                          zaxis = list(title = 'Minimum wage in state ($)')))
-})
+      layout(scene = list(
+        xaxis = list(title = "Years (2001 --- 2017)"),
+        yaxis = list(title = "States"),
+        zaxis = list(title = "Minimum wage in state ($)")
+      ))
+  })
 })
--- ui.R
+++ ui.R
@@ -1,13 +1,151 @@
+introduction <- tabPanel(
+  "Introduction",
+  mainPanel(
+    tags$h4(strong("Information about Minimum Wages",
+                   style = "color: #2C3E50;")),
+    tags$h6(em("June 5th, 2019")),
+    tags$p("The minimum wage in the United States is set by both US and
+           State laws and since July 24, 2009, the minimum wage has been
+           $7.25 an hour. However, recently different states and cities have
+           been experimenting with setting higher minimum wages.
+           Financially, proponents of
+           higher minimum wages believe it will raise worker productivity,
+           reduce turnover, reduce absenteeism, and stimulate the economy. 
+           Meanwhile, those against raising minimum wages believe that
+           raising minimum wages would force businesses to lay off
+           employees, raise unemployment levels, increase poverty, and
+           increase the price of consumer goods. However, minimum wage doens't
+           just have financial effects, but spans into the realm of public
+           health. There are many arguments to
+           be had looking at which groups within the US are disproportionaly
+           affected by a change in minimum wages, and the debate continues
+           even to this day. This project aims to take a broad look at minimum
+           wage and how it might be correlated to obesity rates, education,
+           and homicide rates. However, as each topic is broad and full of
+           multiple caveats, drawing a conclusion is difficult. Instead, this
+           project hopes to open up a conversation regarding minimum wage
+           and encourage conversation into what other topics might be worth
+           looking into. "),
+    tags$h4(strong("Information about the Dataset", style = "color: #2C3E50;")),
+    tags$p(
+      "The information was collected by",
+      em("Lislejoem"), "and sourced by
+          the US Department of Labor's website. The dataset was hosted on Kaggle
+          and is targeted towards
+          individuals who have an interest in seeing the effects of
+          minimum wage. This data set can help answer questions on how minimum
+      wage might affect public health."
+    ),
+    tags$a(
+      href = "https://www.kaggle.com/lislejoem/us-minimum-wage-by-state-from-1968-to-2017",
+      "You can find a link to the data by clicking here."
+    ),
+    tags$h4(strong("Questions that can be asked", style = "color: #2C3E50;")),
+    tags$p("The data set is 2,750 rows long and summarizes information by
+            Year, State, Table Data, Footnote, High.Value (the higher of the
+            minimum wage values provided), Low.Value (the lower of the minimum
+            wage values provided), CPI.Average (Average Consumer Price Index
+            for that year), High.2018 (2018 equivalent dollars for High.Value),
+            and Low.2018 (2018 equivalent dollars for Low.Value).
+            Some of the questions that we
+            attempt to answer with this data set include:"),
+    tags$ol(
+      tags$li("Which state has the highest minimum wage?"),
+      tags$li("How do obesity rates, homicide rates, and education rates
+              link to minimum wage?"),
+      tags$li("Is there a clear connection between more purchasing power
+              and lower crime rates?"),
+      tags$li("How much have education rates been increasing each year?"),
+      tags$li("Which years had the highest/lowest rates of crime?")
+    ),
+    tags$h4(strong("Structure", style = "color: #2C3E50;")),
+    tags$p("The first tab of this report shows has a
+            choropleth map of the minimum wage in each state. 
+            Users can change the year
+            input to see how minimum wages vary in each
+            state from 1968 to 2017.
+            The second page shows homicide rates graphed by year as well as a
+            plot of 
+            homicide rates graphed by minimum wages.
+            Users can select the state
+            and see how the homicide rate for that state over time as well as
+            how the minimum wage in 2018 dollars is correlated with homicide
+            rates.
+            The third plot is a 3D plot that 
+            that looks at obesity/overweight statistics and the minimum wage
+            plotted against time from 2001 - 2017.
+           Users can select which state(s) they want to analyze and see general
+           trends of obesity rates and the minimum wage."),
+    tags$h4(strong("Creators", style = "color: #2C3E50;")),
+    tags$ol(
+      tags$li("Steven Hsieh"),
+      tags$li("Jerry Lin"),
+      tags$li("Steve Ma"),
+      tags$li("Weixing Nie")
+    )
+  )
+)
+
 plotly_wage <- tabPanel(
     "Minimum Wage Map",
     titlePanel("Minimum Wage Choropleth by State and Year"),
     sidebarLayout(
         sidebarPanel(
             sliderInput("year_slider", label = h3("Select Year"), min = 1968, 
-                        max = 2017, value = 1993, sep = "", animate = TRUE)
+                        max = 2017, value = 2017, sep = "", animate = TRUE)
         ),
         mainPanel(
-            plotlyOutput("plotlymap")
+          tags$h4(strong("Minimum Wage Choropleth "), align = "center"),
+            plotlyOutput("plotlymap"),
+          tags$p("This choropleth shows the minimum wage for each state in the
+          United States from 1968-2017. Each state is graphed on this plot
+          including Alaska and Hawaii. Keep in mind that some states do not
+          have an official minimum wage or have state minimum wages
+          less than federal minimum wage levels. If the federal wage is higher
+          than the state wage, we have replaced the state wage with the federal
+          level as that is the wage they would have been paid. Often states with
+          lower documented minimum wages do not update their wage to reflect
+          the federal level as the employees are being paid the federal level
+          anyways, and updating the wage would be an inefficient use of
+                 resources."),
+          tags$p(
+            strong("To use this map:"), "Look at the side panel on the
+              top left side
+              of the page. You can select the year of interest with the",
+            strong("Select Year"), "slider. Once you select the year,
+              the choropleth will display
+              the information for the minimum wage of that year. Upon hovering
+            over each state, detailed information will be shown including the
+            Low.Value for that state, the High.Value adjusted for 2018 dollars,
+            and the Low.Value adjusted for 2018 dollars. You can also
+            press the play button located in the bottom right of the Select Year
+            slider to have the plot automatically progress through each year."
+          ),
+          tags$h5(strong(em("Possible Questions You Could Ask About the Plot:",
+                            style = "color: #2C3E50;"
+          ))),
+          tags$ol(
+            tags$li("Which state has the highest minimum wage in 2017?"),
+            tags$li("When did states begin to enact their own minimum wages
+                    different from federal levels?"),
+            tags$li("How have specific minimum wage values changed over time
+                    to match with inflation?")
+          ),
+          tags$h5(
+            strong("Insights:", style = "color: #2a8e0e;")
+          ),
+          tags$p("Generally, most of the states have the same minimum wage as
+                 set by the federal level from 1968 to about the late 1990s.
+                 However, once the 2000s come, we see more segmentation in
+                 states and their minimum wage. Interestingly, Alaska seems to
+                 be a pioneer of setting higher minimum wages than the federal
+                 average. Once the time period is near 2010, the west coast
+                 take a lead in setting higher than average minimum wages, with
+                 the east coast soon following. However, much of the deep South
+                 and areas surrounding Texas often stick to the federal minimum
+                 wage. Surprisingly, in 1985, the federal minimum wage was only
+                 $3.35, which speaks to how inflation has changed this wage over
+                 time.")
         )    
 ))
 
@@ -15,29 +153,82 @@ homicide_graph <- tabPanel(
     "Homicide",
     titlePanel("Homicide Rates Graphed by State and Year"),
     sidebarLayout(
+      sidebarPanel(
         selectizeInput("state_text_name", "Select State",
                     state_list
-        ),
-    mainPanel(
-        plotlyOutput("homicide_graph")
-    )
-))
-
-homicide_wage <- tabPanel(
-  "Homicide and Wage",
-  titlePanel("Homicide Rates and Minimum Wage Graphed Together"),
-  sidebarLayout(
-    selectizeInput("state_hom_wage", "Select State",
-                   state_list
+        )),
+    mainPanel(tags$h4(strong("Homicide Rate Plot"), align = "center"),
+              div(class = "span6", plotlyOutput("homicide_graph")),
+              tags$h4(strong("Homicide vs Minimum Wage Plot"), 
+                      align = "center"),
+              div(class = "span6", plotlyOutput("p_hom_wage")),
+    tags$p("These two plots demonstrate homicide rates over time per state as
+    well as those same homicide rates over time plotted against the minimum wage
+    adjusted for 2018 dollars of that state. For consistency, homicide rates
+    have been plotted on the Y-Axis. In the first plot, the year has been
+    plotted on the X-Axis. In the second plot, adjusted 2018 minimum wages have
+    been plotted on the X-Axis."),
+    tags$p(
+      strong("To use this map:"), "Look at the side panel on the
+              top left side
+              of the page. You can select the state of interest with the",
+      strong("Select State"), "drop-down menu. Once you select the state,
+              the two plots will display
+              the information for the homicide rates of that state. Both plots
+      will automatically update for the requested state when selected."
     ),
-    mainPanel(
-      plotlyOutput("p_hom_wage")
+    tags$h5(strong(em("Possible Questions You Could Ask About these Plots:",
+                      style = "color: #2C3E50;"
+    ))),
+    tags$ol(
+      tags$li("Which year had the highest homicide rates for X State?"),
+      tags$li("Is there a strong correlation between adjusted minimum wages
+              and the homicide rate? How does this correlation change
+              between states?"),
+      tags$li("Why is the Homicide vs. Minimum Wage plot considerably messier
+              than the Homicide Rate Plot? What might this suggest?")
+    ),
+    tags$h5(
+      strong("Insights:", style = "color: #2a8e0e;")
+    ),
+    tags$p("Each state often paints a starkly different picture when it comes to
+           how homicide rates have changed throughout the years. While states
+           like South Carolina and Washington have similar trend lines where
+           they sharply rise until about 1993, and then begin to decline, states
+           like North Dakota have experienced a rapid explosion in homicide
+           rates
+           in the last few years of the plot. States like New York have done an
+           excellent job bringing homicide rates but Vermont has not experienced
+           the same level of success.
+           Meanwhile, when looking at the Homicide vs. Minimum Wage plot, we can
+           see that generally as minimum wage increases (as we go farther right
+           on the plot), the homicide rate will decrease (go down on the plot).
+           However, this correlation doesn't necessarily mean causation. Crime
+           rates have on average been decreasing throughout the years, and
+           many states have been raising their minimum wage. Despite this, each
+           state paints a rather fascinating picture of their homicide and
+           minimum wage, and instead of looking at the plots as a whole, it
+           might be more useful to compare states to states.")
+    
     )
-  ))
+    )
+)
+# homicide_wage <- tabPanel(
+#   "Homicide and Wage",
+#   titlePanel("Homicide Rates and Minimum Wage Graphed Together"),
+#   sidebarLayout(
+#     selectizeInput("state_hom_wage", "Select State",
+#                    state_list
+#     ),
+#     mainPanel(
+#       plotlyOutput("p_hom_wage")
+#     )
+#   ))
 
+### Creating 3d Plot tabPanel
 shiny_3d <- tabPanel(
-    "Obesity, Overweight and Minimum Wage",
-    titlePanel(h1("Obesity, Overweight and Minimum Wage")),
+    "Obesity and Minimum Wages",
+    titlePanel(h1("Obesity/Overweight Percentages and Minimum Wage")),
     sidebarLayout(
       sidebarPanel(
         checkboxGroupInput(
@@ -47,16 +238,120 @@ shiny_3d <- tabPanel(
           selected = unique(minimum_wage$State)
         )
       ),
-      mainPanel(plotlyOutput("statee"))
+      mainPanel(tags$h4(strong("3D Plot"), align = "center"),
+        plotlyOutput("statee"),tags$p("UPDATE THIS: WHAT DOES THIS 3D plot SHOW?
+        These two plots demonstrate homicide rates over time per state as
+    well as those same homicide rates over time plotted against the minimum wage
+    adjusted for 2018 dollars of that state. For consistency, homicide rates
+    have been plotted on the Y-Axis. In the first plot, the year has been
+    plotted on the X-Axis. In the second plot, adjusted 2018 minimum wages have
+    been plotted on the X-Axis."),
+        tags$p(
+          strong("To use this map:"), "UPDATE THIS:Look at the side panel on the
+              top left side
+              of the page. You can select the state of interest with the",
+          strong("Select State"), "drop-down menu. Once you select the state,
+              the two plots will display
+              the information for the homicide rates of that state. Both plots
+      will automatically update for the requested state when selected."
+        ),
+        tags$h5(strong(em("Possible Questions You Could Ask About these Plots:",
+                          style = "color: #2C3E50;"
+        ))),
+        tags$ol(
+          tags$li("QUESTION 1"),
+          tags$li("QUESTION 2"),
+          tags$li("QUESTION 3")
+        ),
+        tags$h5(
+          strong("Insights:", style = "color: #2a8e0e;")
+        ),
+        tags$p("INSIGHT HERE - CHECK WHAT I'VE WRITTEN IN THE OTHER SECTIONS"))
     )
   )
 
+### About Us tabPanel
+about_us <- tabPanel(
+  "Meet the Team",
+  headerPanel(
+    h4("About Us",
+       style = "font-family: 'Arial';cursive;
+         font-size: 25px; line-height: 1.0:
+         color: #36454f;
+         text-align: center;
+         font-weight: 300;"
+    )
+  ),
+  tags$h4("This project was created by a team of four students in INFO 478
+            at the University of Washington. It was created by",
+          strong("Steven Hsieh,
+        Jerry Lin,
+        Steve Ma,
+        and Weixing Nie."),
+          align = "center"
+  ),
+  sidebarPanel(
+    tags$h4(strong("Steven Hsieh"), align = "middle"),
+    tags$img(
+      src = "http://tinyurl.com/y2m2n76j", height = "200 px",
+      style = "display: block; margin-left: auto; margin-right: auto;"
+    ),
+    tags$p("Steven Hsieh is a Junior at the University of Washington
+            studying Finance. With R, he wishes to make pretty
+            graphs and cool visualizations that also show information in
+            a meaningful way.")
+  ),
+  sidebarPanel(
+    tags$h4(strong("Jerry Lin"), align = "center"),
+    tags$img(
+      src = "https://i.imgur.com/pymLtTS.png", height = "200 px",
+      style = "display: block; margin-left: auto; margin-right: auto;"
+    ),
+    tags$p("Jerry Lin is a sophomore majoring in Informatics with an intended
+           concentration in data science.  In the future, Jerry hopes to ideally
+           work in the field of computational linguistics or be a code monkey.
+           During his spare time, Jerry enjoys playing video games, studying
+           linguistics, and reading books.")
+  ),
+  sidebarPanel(
+    tags$h4(strong("Steve Ma"), align = "center"),
+    tags$img(
+      src = "https://i.imgur.com/6ydZDZn.png", height = "200 px",
+      style = "display: block; margin-left: auto; margin-right: auto;"
+    ),
+    tags$p("Steve Ma is a UW Sophomore majoring in Applied & Computational
+           Mathematical Science with a focus in Scientific Computing,
+           Numerical Algorithms and Computer Science. In the future, Steve wants
+           to work in the aspect of Applied Math, while simultaneously
+           introducing younger students to the basics of coding and setting up
+           a strong foundation in Math.")
+  ),
+  sidebarPanel(
+    tags$h4(strong("Weixing Nie"), align = "center"),
+    tags$img(
+      src = "https://i.imgur.com/19gQVEF.jpg", height = "200 px",
+      style = "display: block; margin-left: auto; margin-right: auto;"
+    ),
+    tags$p("Oliver Nie/Weixing Nie is currently an undergraduate student of UW.
+           He is interested in data science, which is a reason why he wishes
+           to study informatics. Weixing believes in the power of data science 
+           to correct contemporary mistakes of human beings. His dream job is to
+           become a video game director, and he truly wants to develop a game
+           that can be recognized as the candidate for Nobel Literature Prize.
+           In his vision, the future of video games is a small scale of a
+           reality simulation.")
+  )
+)
+
 shinyUI(navbarPage(
     # includeCSS("style.css"),
-    theme = shinytheme("spacelab"),
+    theme = shinytheme("flatly"),
     "An Analysis of Minimum Wage",
+    introduction,
     plotly_wage,
     homicide_graph,
-    homicide_wage,
-    shiny_3d
+    shiny_3d,
+    about_us
 ))
+
+
